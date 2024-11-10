fea61ff17736c04759c65ff88bd75f2662282935
Roope Hakulinen <roope.hakulinen@gmail.com>
2024-11-10 13:36:28 +0200

    chore: fix the database service creation

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { FriendsModule } from './friends/friends.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [FriendsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
