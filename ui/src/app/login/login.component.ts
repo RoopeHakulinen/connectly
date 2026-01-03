0776a22960baaf3d1887922f73b9ad1c93036aa3
Roope Hakulinen <roope.hakulinen@gmail.com>
2026-01-03 13:52:35 +0200

    Add language selection.

--- ui/src/app/login/login.component.ts
+++ ui/src/app/login/login.component.ts
@@ -1,18 +1,28 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatIconModule } from '@angular/material/icon';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { AuthService } from '../auth.service';
 
 @Component({
     selector: 'app-login',
-    imports: [MatIconModule, TranslateModule],
+  imports: [MatButtonToggleModule, MatIconModule, TranslateModule],
     templateUrl: './login.component.html',
     styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   private authService = inject(AuthService);
+  private translateService = inject(TranslateService);
+
+  currentLanguage = this.translateService.currentLang || 'en';
 
   login(): void {
     this.authService.login();
+  }
+
+  changeLanguage(lang: string): void {
+    this.currentLanguage = lang;
+    this.translateService.use(lang);
+    localStorage.setItem('selectedLanguage', lang);
   }
 }
--- ui/src/app/profile/profile.component.ts
+++ ui/src/app/profile/profile.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [MatButtonToggleModule, MatCardModule, TranslateModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
-export class ProfileComponent {}
+export class ProfileComponent {
+  private translateService = inject(TranslateService);
+
+  currentLanguage = this.translateService.currentLang || 'en';
+
+  changeLanguage(lang: string): void {
+    this.currentLanguage = lang;
+    this.translateService.use(lang);
+    localStorage.setItem('selectedLanguage', lang);
+  }
+}
