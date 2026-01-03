ba8554591d6bfadfbc0eb4af2c732d437dd208a2
Roope Hakulinen <roope.hakulinen@gmail.com>
2026-01-03 14:29:53 +0200

    Upgrade part of the Angular 20.

--- ui/src/app/record-activity-dialog/record-activity-dialog.component.ts
+++ ui/src/app/record-activity-dialog/record-activity-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
@@ -27,7 +27,6 @@ export interface RecordActivityDialogResult {
   selector: 'app-record-activity-dialog',
   standalone: true,
   imports: [
-    CommonModule,
     ReactiveFormsModule,
     MatDialogModule,
     MatButtonModule,
