c263dd204ff0fdd7e9db5d9da995e3ec75cb042b
Vili Mäkinen <makinen@gmail.com>
2026-01-03 17:12:06 +0200

    Clean up some files.

--- start.sh
+++ start.sh
@@ -9,9 +9,4 @@ npx prisma migrate deploy
 
 # Start the application
 echo "Starting NestJS application..."
-echo "Current Directory: $(pwd)"
-echo "Listing /home/node/dist:"
-ls -la /home/node/dist
-echo "Finding index.html..."
-find /home/node -name "index.html"
 exec node dist/src/main.js
