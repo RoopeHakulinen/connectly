804997f9a46c8a931c16a7ba11eae922196de40d
Vili Mäkinen <makinen@gmail.com>
2026-01-16 15:10:47 +0200

    Update the manifest.

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,4 +3,9 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js');
+    }
+  })
   .catch((err) => console.error(err));
